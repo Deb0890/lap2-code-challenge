04899fbd34c3cf189b031b92dac8d93d472926c8
deb0890 <deborahf1@hotmail.com>
2021-07-20 15:57:19 +0100

    creating entry models

--- api/models/entries.js
+++ api/models/entries.js
@@ -1,5 +1,33 @@
-const { Pool } = require("pg");
+const db = require ('../dbConfig')
 
-const pool = new Pool();
+class Entry {
+    constructor(data){
+        this.id = data.id
+        this.title = data.title
+        this.name = data.name
+        this.story = data.story
+    }
 
-module.exports = pool;
+    static get all() {
+        return new Promise (async (resolve, reject) => {
+            try {
+                const entriesData = await db.query(`SELECT * FROM entries;`)
+                const entries = entriesData.rows.map(d = new Entry(d))
+                resolve(entries);
+            } catch (err) {
+                reject("Error retrieving entries")
+            }
+        })
+    }
+
+    static findById (id) {
+        return new Promise (async (resolve, reject) => {
+            try {
+                let entryData = await db.query(`SELECT * FROM entries WHERE id = $1;`, [ id ]);
+                let entry = new Entry(entryData.rows[0]);
+                resolve (entry);
+            }
+        })
+    }
+
+}
