938206cc7204f372bb5e1bef57d4a68e0b7696ad
deb0890 <deborahf1@hotmail.com>
2021-07-20 17:10:42 +0100

    create routers

---
+++ api/controllers/Entries.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+
+const Entry = require('../models/entries')
+
+//Get all entries route
+router.get('/', async (req, res) => {
+    try {
+        const entries = await Entry.all
+        res.json({entries})
+    } catch(err) {
+        res.status(500).json({err})
+    }
+})
+
+//Get one entry route
+router.get('/:id', async (req, res) => {
+    try {
+        const entry = await Entry.findById(parseInt(req.params.id))
+        res.json(entry)
+    } catch(err) {
+        res.status(404).json({err})
+    }
+})
+
+//create entry route
+router.post('/', async (req, res) => {
+    try {
+        const entry = await Entry.create(req.body.title, req.body.name, req.body.story)
+        res.json(entry)
+    } catch(err) {
+        res.status(404).json({err})
+    }
+})
